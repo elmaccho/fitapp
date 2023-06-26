b68fb6d6ed244737f678ba8ed6aa8d8fd209675a
elmaccho <maciek.chojnacki22@wp.pl>
2023-06-26 23:24:47 +0200

    search

--- src/Components/ItemSearch/ItemSearch.js
+++ src/Components/ItemSearch/ItemSearch.js
@@ -1,11 +1,26 @@
 import React from "react";
 import './style.css'
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+
 function ItemSearch(){
     return(
         <div className="ItemSearch-wrapper">
-            <div className="Search-pannel"></div>
-            <div className="Search-list"></div>
+            <button className="retBtn">
+                <FontAwesomeIcon icon={faArrowLeft}></FontAwesomeIcon>
+            </button>
+
+            <div className="Search-pannel">
+                <input className="SearchInput" type="text" placeholder="Wyszukaj produkt..."></input><button className="SearchBtn" >Szukaj</button>
+            </div>
+            <div className="Search-list">
+                <span>eeeeeeeeeeeeeeeeeeee</span>
+                <span>eeeeeeeeeeeeeeeeeeee</span>
+                <span>eeeeeeeeeeeeeeeeeeee</span>
+                <span>eeeeeeeeeeeeeeeeeeee</span>
+                <span>eeeeeeeeeeeeeeeeeeee</span>
+            </div>
         </div>
     )
 }
